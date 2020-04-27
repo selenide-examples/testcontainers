19482c25cd89f7fe8f8baede8abea78c1e784462
Denis Gaievskyi <denis.gayevskiy@smartbear.com>
2020-04-27 17:58:58 +0300

    add example with Proxy download

---
+++ src/test/java/org/selenide/examples/DownloadTestWithDockerAndProxy.java
@@ -0,0 +1,71 @@
+package org.selenide.examples;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.FileDownloadMode;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.impl.StaticConfig;
+import com.codeborne.selenide.proxy.SelenideProxyServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.FileFilters.withName;
+import static org.junit.Assert.assertTrue;
+
+public class DownloadTestWithDockerAndProxy {
+
+    private static final int proxyPort = 8864;
+
+    private SelenideProxyServer proxyServer;
+
+    static {
+        Testcontainers.exposeHostPorts(proxyPort);
+    }
+
+    //if your baseUrl is http, you'll need to use new Proxy().setHttpProxy
+    @Rule
+    public BrowserWebDriverContainer chrome =
+            new BrowserWebDriverContainer()
+                    .withCapabilities(new ChromeOptions().setProxy(new Proxy()
+                            .setSslProxy("host.testcontainers.internal:" + proxyPort))
+                            .setAcceptInsecureCerts(true));
+
+    @Before
+    public void setUp() {
+        Configuration.proxyHost = "host.testcontainers.internal";
+        Configuration.proxyPort = proxyPort;
+        Configuration.proxyEnabled = true;
+        Configuration.fileDownload = FileDownloadMode.PROXY;
+
+        proxyServer = new SelenideProxyServer(new StaticConfig(), null);
+        proxyServer.start();
+
+        RemoteWebDriver driver = chrome.getWebDriver();
+        WebDriverRunner.setWebDriver(driver, proxyServer);
+    }
+
+    @After
+    public void tearDown() {
+        proxyServer.shutdown();
+        WebDriverRunner.closeWebDriver();
+    }
+
+    @Test
+    public void search() throws FileNotFoundException {
+        open("https://mvnrepository.com/artifact/com.codeborne/selenide/5.11.1");
+
+        File selenideJar = $("table.grid a.vbtn").download(withName("selenide-5.11.1.jar"));
+        assertTrue(selenideJar.exists());
+    }
+}
