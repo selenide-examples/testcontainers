c0e6dcd19162a4a7d3ea6617b096b13d66344b0d
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-04-28 09:48:22 +0300

    reset config after test

--- src/test/java/org/selenide/examples/DownloadTestWithDockerAndProxy.java
+++ src/test/java/org/selenide/examples/DownloadTestWithDockerAndProxy.java
@@ -18,6 +18,8 @@ import org.testcontainers.containers.BrowserWebDriverContainer;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import static com.codeborne.selenide.FileDownloadMode.HTTPGET;
+import static com.codeborne.selenide.FileDownloadMode.PROXY;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.files.FileFilters.withName;
@@ -46,7 +48,7 @@ public class DownloadTestWithDockerAndProxy {
         Configuration.proxyHost = "host.testcontainers.internal";
         Configuration.proxyPort = proxyPort;
         Configuration.proxyEnabled = true;
-        Configuration.fileDownload = FileDownloadMode.PROXY;
+        Configuration.fileDownload = PROXY;
 
         proxyServer = new SelenideProxyServer(new StaticConfig(), null);
         proxyServer.start();
@@ -55,17 +57,21 @@ public class DownloadTestWithDockerAndProxy {
         WebDriverRunner.setWebDriver(driver, proxyServer);
     }
 
-    @After
-    public void tearDown() {
-        proxyServer.shutdown();
-        WebDriverRunner.closeWebDriver();
-    }
-
     @Test
     public void search() throws FileNotFoundException {
         open("https://mvnrepository.com/artifact/com.codeborne/selenide/5.11.1");
 
         File selenideJar = $("table.grid a.vbtn").download(withName("selenide-5.11.1.jar"));
         assertTrue(selenideJar.exists());
+    }
+
+    @After
+    public void tearDown() {
+        proxyServer.shutdown();
+        WebDriverRunner.closeWebDriver();
+        Configuration.fileDownload = HTTPGET;
+        Configuration.proxyEnabled = false;
+        Configuration.proxyHost = null;
+        Configuration.proxyPort = 0;
     }
 }
